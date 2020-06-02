8712d25ac6f59d7ea2aceee81dd1f7d9ca05956f
JaderSabino <jader.sabino2014@gmail.com>
2020-06-02 00:00:47 -0300

    Finalizando clone Pipefy

---
+++ recriando-pipefy/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default props =>
+    <>
+        <Container>
+            <h1>Clone Pipefy</h1>
+        </Container>
+    </>
